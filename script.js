ae1357ff563304154a62ac007fa069c26acd5fd5
Itter Scalvenzi <itterscalvenzi15@gmail.com>
2022-09-20 11:36:35 -0300

    clase 03 04 y 05 completadas

--- script.js
+++ script.js
@@ -1,38 +1,11 @@
-import checkComplete from "./components/checkComplete.js";
-import deleteIcon from "./components/deleteIcon.js";
+import {addTask} from "./components/addTask.js"
+import { displayTask } from "./components/readTasks.js"
+
 ( () => {
 const btn = document.querySelector("[data-form-btn]");
-
-const createTask = (evento) =>{
-    evento.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement("li");
-    task.classList.add("card");
-    input.value = "";
-    //backticks
-    const taskContent = document.createElement("div");
-   
-    taskContent.appendChild(checkComplete());
-    const titleTask = document.createElement("span");
-    titleTask.classList.add("task");
-    if(value.length == 0){
-        titleTask.innerText = value;
-    }
-    
-    taskContent.appendChild(titleTask);
-    
-    //task.innerHTML = content;
-    task.appendChild(taskContent);
-    task.appendChild(deleteIcon());
-    list.appendChild(task);
-};
  
-btn.addEventListener("click", createTask);
+btn.addEventListener("click", addTask);
 
-
-
-
+displayTask();
 
 })();
